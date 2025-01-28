87bf644095b39a684cc18b5593fb538ef422bd70
arnav-umika <156506711+arnav-umika@users.noreply.github.com>
2025-01-29 04:00:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+<html>
+ <head>
+  <title>
+   Good Morning
+  </title>
+  <script src="https://cdn.tailwindcss.com">
+  </script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
+  <style>
+   @import url('https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:wght@400;700&display=swap');
+        .font-pacifico {
+            font-family: 'Pacifico', cursive;
+        }
+        .font-roboto {
+            font-family: 'Roboto', sans-serif;
+        }
+  </style>
+ </head>
+ <body class="bg-blue-200 flex items-center justify-center min-h-screen">
+  <div class="text-center">
+   <div class="relative inline-block">
+    <img alt="Decorative circular design" class="absolute top-0 left-0 w-40 h-40 transform -translate-x-1/2 -translate-y-1/2" src="https://media.discordapp.net/attachments/1222887410602344512/1333921010008719452/ae24IOAKrQMCJehVu9kU6Ao7AkhFwol6y99x2R8ARWAUCTtSrcLMv0hFwBJaMgBP1kr3ntjsCjsAqEHgDr3GCs8oSmewAAAAASUVORK5CYII.png?ex=679aa63f&is=679954bf&hm=965ca004c5185a3062df3c3dbec79e1413855b1f7aede255b9466a20761f818e&=&format=webp&quality=lossless&width=724&height=684"/>
+    <img alt="Decorative circular design" class="absolute bottom-0 right-0 w-40 h-40 transform translate-x-1/2 translate-y-1/2" src="https://media.discordapp.net/attachments/1222887410602344512/1333921010008719452/ae24IOAKrQMCJehVu9kU6Ao7AkhFwol6y99x2R8ARWAUCTtSrcLMv0hFwBJaMgBP1kr3ntjsCjsAqEHgDr3GCs8oSmewAAAAASUVORK5CYII.png?ex=679aa63f&is=679954bf&hm=965ca004c5185a3062df3c3dbec79e1413855b1f7aede255b9466a20761f818e&=&format=webp&quality=lossless&width=724&height=684"/>
+   </div>
+   <div class="relative z-10">
+    <div class="flex justify-center items-center mb-4">
+     <span class="text-6xl font-pacifico text-black">
+      Good Morning
+     </span>
+     <img alt="Cup of tea with steam" class="w-36 h-36 ml-2" src="https://media.discordapp.net/attachments/1222887410602344512/1333921010008719452/ae24IOAKrQMCJehVu9kU6Ao7AkhFwol6y99x2R8ARWAUCTtSrcLMv0hFwBJaMgBP1kr3ntjsCjsAqEHgDr3GCs8oSmewAAAAASUVORK5CYII.png?ex=679aa63f&is=679954bf&hm=965ca004c5185a3062df3c3dbec79e1413855b1f7aede255b9466a20761f818e&=&format=webp&quality=lossless&width=724&height=684"/>
+    </div>
+    <div class="text-6xl font-pacifico text-black mb-4">
+    My Lovely Girlfriend 
+    </div>
+    </div>
+    <div class="text-xl font-roboto text-black mt-2 leading-relaxed">
+     I love you so so so much. I hope you have an awesome day. 
+    </div>
+   </div>
+  </div>
+ </body>
+</html>
